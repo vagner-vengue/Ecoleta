408c62bef1ef9b59f9bbf72a06b5551235d969c3
Vagner Vengue <vagnervengue@users.noreply.github.com>
2020-09-16 22:20:04 -0300

    Commit Release-1.0.1.

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
-import { API_URL } from 'react-native-dotenv';
+import { API_URL, API_URL_QA, DEBUG_MODE } from 'react-native-dotenv';
+
+const URL = (DEBUG_MODE=='true') ? API_URL_QA : API_URL;
 
 const api = axios.create({
     // The address of your API server.
-    baseURL: API_URL
+    baseURL: URL
 });
 
 export default api;
